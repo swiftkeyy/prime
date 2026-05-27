0f4fdca2bee9f2b4a5a71061e7b91e4c312834a2
swiftkeyy <pp775801@gmail.com>
2026-05-27 16:37:07 +0400

    Fix double app import on Railway

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ async def on_shutdown() -> None:
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", settings.PORT)), proxy_headers=True)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", settings.PORT)), proxy_headers=True)
